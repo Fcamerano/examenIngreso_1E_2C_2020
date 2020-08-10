93be42502544ba7ab5e9dc536a521bff3aed2dfa
Francisco <francamerano@gmail.com>
2020-08-10 20:52:40 -0300

    Ultima actualización

--- dos.js
+++ dos.js
@@ -5,35 +5,58 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar()
+*/
+function mostrar()
 {
   let marca;
   let precio;
   let peso;
   let tipo;
   let respuesta;
-
-
-
+  let acumuladorPeso = 0;
+  let liquidoMasCaro = 0;
+  let marcaLiquidoMasCaro = "";
+  let flagSolido = 0;
+  let solidoMasBarato = 0;
+  let marcaSolidoMasBarato = "";
 
   do
   {
     marca = prompt("Ingrese la marca:");
     precio = parseInt(prompt("Ingrese el precio:"));
+    peso = parseInt(prompt("Ingrese el peso:"));
 
+    do
+		{
+			tipo = prompt("Ingrese el tipo : solido/liquido");
+		}while(tipo != "solido" && tipo != "liquido");
 
+    acumuladorPeso = acumuladorPeso + peso; 
 
+    switch(tipo)
+		{
+			case "liquido":
+        if(precio > liquidoMasCaro)
+				{
+          liquidoMasCaro = precio;
+          marcaLiquidoMasCaro = marca;
+        }
+        break;
+			case "solido":
+        if(flagSolido == 0 || solidoMasBarato > precio)
+				{
+					precioAlcoholbarato = precio;
+					marcaSolidoMasBarato = marca;
+					flagSolido = 1;
+				}
+        break;
+    }
 
     respuesta = prompt("Quiere seguir ingresando? si/no");
   }while(respuesta == "si");
 
 
-
-
-
-
-
-
-
-
+  alert("El peso total es: " + acumuladorPeso); // A)
+  alert("La marca mas cara de los liquidos es: " + marcaLiquidoMasCaro); // B)
+  alert("La marca mas barata de los solidos es: " + marcaSolidoMasBarato); // C)
 }
--- tres.js
+++ tres.js
@@ -9,7 +9,93 @@ a)el lugar más elegido
 b)el sexo de titular que lleva más pasajeros.
 c)el promedio de personas por viaje,  que viajan en invierno
 */
-function mostrar()
-{
-	alert("tres");
+function mostrar() {
+	let sexo;
+	let lugar;
+	let temporada;
+	let personas;
+	let respuesta;
+	let contadorBariloche = 0;
+	let contadorCataratas = 0;
+	let contadorSalta = 0;
+	let sexoConMasPasajeros;
+	let mayorCantidadDePasajeros = 0;
+	let acumuladorPersonas = 0;
+	let viajesInvierno = 0;
+	let promedioInvierno = 0;
+	
+
+	do {
+
+
+
+		do {
+			sexo = prompt("Ingrese su sexo: ");
+		} while (sexo != "f" && sexo != "m");
+
+		do {
+			lugar = prompt("Ingrese el destino: ");
+		} while (lugar != "bariloche" && lugar != "cataratas" && lugar != "salta");
+
+		do {
+			temporada = prompt("Ingrese la temporada: ");
+		} while (temporada != "otoño" && temporada != "invierno" && temporada != "verano" && temporada != "primavera");
+
+		personas = parseInt(prompt("Ingrese la cantidad de personas: "));
+
+
+
+
+		switch (lugar) {
+			case "bariloche":
+				contadorBariloche++;
+				break;
+
+			case "cataratas":
+				contadorCataratas++;
+				break;
+
+			case "salta":
+				contadorSalta++;
+				break;
+		}
+
+
+
+
+
+		if (mayorCantidadDePasajeros < personas) {
+			mayorCantidadDePasajeros = personas;
+			sexoConMasPasajeros = sexo;
+		}
+
+		if (temporada == "invierno") {
+			acumuladorPersonas = acumuladorPersonas + personas;
+			viajesInvierno++;
+		}
+
+		respuesta = prompt("Quiere ingresar otra? si/no");
+
+	} while (respuesta == "si");
+
+
+	promedioInvierno = acumuladorPersonas / viajesInvierno;
+
+
+
+    // A)
+	if (contadorBariloche > contadorCataratas && contadorBariloche > contadorSalta) {
+		alert("El lugar mas visitado es Bariloche");
+	}
+	else if (contadorCataratas > contadorBariloche && contadorCataratas > contadorSalta) {
+		alert("El lugar mas visitado es Cataratas");
+	}
+	else {
+		alert("El destino mas visitado es Salta");
+	}
+
+
+
+	alert("El sexo del titular con mas pasajeros es " + sexoConMasPasajeros); // B)
+	alert("El promedio de personas que viajan en invierno es de: " + promedioInvierno);// C)
 }
